7c51108b0826db7c01803f23f40a3fd66dea16c0
kampffrosch94 <kampffrosch94@github.com>
2023-06-24 12:21:56 +0200

    reformat

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,27 @@
 <script lang="ts">
     interface Deck {
-        name: string,
-        id: number,
-        known_coverage: number,
-        learning_coverage: number,
-        is_built_in: boolean
+        name: string;
+        id: number;
+        known_coverage: number;
+        learning_coverage: number;
+        is_built_in: boolean;
     }
 
-    let token: string = ""
-    let result: string = ""
-    let decks: Deck[] = []
-    let min_coverage = 80
-    let min_learning = 80
-    let filter_builtin = false
+    let token: string = "";
+    let result: string = "";
+    let decks: Deck[] = [];
+    let min_coverage = 80;
+    let min_learning = 80;
+    let filter_builtin = false;
 
     async function doPing() {
         let headers = new Headers();
-        headers.set('Authorization', "Bearer " + token);
-        headers.set('Content-Type', 'application/json');
-        const res = await fetch('https://jpdb.io/api/v1/ping', {
+        headers.set("Authorization", "Bearer " + token);
+        headers.set("Content-Type", "application/json");
+        const res = await fetch("https://jpdb.io/api/v1/ping", {
             headers: headers,
-            method: 'POST',
-            body: JSON.stringify({})
+            method: "POST",
+            body: JSON.stringify({}),
         });
 
         const json = await res.json();
@@ -30,44 +30,48 @@
 
     async function fetchDecks() {
         let headers = new Headers();
-        headers.set('Authorization', "Bearer " + token);
-        headers.set('Content-Type', 'application/json');
-        const res = await fetch('https://jpdb.io/api/v1/list-user-decks', {
+        headers.set("Authorization", "Bearer " + token);
+        headers.set("Content-Type", "application/json");
+        const res = await fetch("https://jpdb.io/api/v1/list-user-decks", {
             headers: headers,
-            method: 'POST',
+            method: "POST",
             body: JSON.stringify({
-                fields: ["name", "vocabulary_known_coverage", "vocabulary_in_progress_coverage", "is_built_in", "id"]
-            })
+                fields: [
+                    "name",
+                    "vocabulary_known_coverage",
+                    "vocabulary_in_progress_coverage",
+                    "is_built_in",
+                    "id",
+                ],
+            }),
         });
 
         const json = await res.json();
         result = JSON.stringify(json);
-        decks = json.decks.map((it:[string, number, number, boolean, number]) => {
-            let [name, known_coverage, learning_coverage, is_built_in, id] = it;
-            return {
-                name: name,
-                id: id,
-                known_coverage: known_coverage,
-                learning_coverage: learning_coverage,
-                is_built_in: is_built_in
+        decks = json.decks.map(
+            (it: [string, number, number, boolean, number]) => {
+                let [name, known_coverage, learning_coverage, is_built_in, id] =
+                    it;
+                return {
+                    name: name,
+                    id: id,
+                    known_coverage: known_coverage,
+                    learning_coverage: learning_coverage,
+                    is_built_in: is_built_in,
+                };
             }
-        });
-        decks.sort((a: Deck, b: Deck) => a.name.localeCompare(b.name) ); // sort by name
+        );
+        decks.sort((a: Deck, b: Deck) => a.name.localeCompare(b.name)); // sort by name
     }
-
 </script>
-
 
 <input bind:value={token} type="password" />
 <button type="button" on:click={doPing}>Ping</button>
 
-<p>
-    Result:
-</p>
+<p>Result:</p>
 <pre>
     {result}
 </pre>
-
 
 <button type="button" on:click={fetchDecks}>Fetch user decks</button>
 Min coverage:
@@ -79,7 +83,7 @@ Filter builtin:
 
 {#each decks as deck}
     {#if deck.known_coverage > min_coverage && deck.learning_coverage > min_learning && (deck.is_built_in || !filter_builtin)}
-        <pre> <a href="https://jpdb.io/deck?id={deck.id}">{deck.name}</a>
+        <pre> {deck.name} <a href="https://jpdb.io/deck?id={deck.id}">[x]</a>
         Coverage: {deck.known_coverage}
         Learning Coverage: {deck.learning_coverage}
         Builtin: {deck.is_built_in}
