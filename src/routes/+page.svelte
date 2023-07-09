a7436c27837f3acf7bca862033a41b1239a80901
kampffrosch94 <kampffrosch94@github.com>
2023-07-09 13:59:48 +0200

    added deck merging

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,24 @@
         is_built_in: boolean;
     }
 
+    interface Vocab {
+        vid: number;
+        sid: number;
+        occurences: number;
+    }
+
+    interface DeckWithVocab extends Deck {
+        vocabs: Vocab[];
+    }
+
     let token: string = "";
     let result: string = "";
     let decks: Deck[] = [];
+    let selected_decks: Deck[] = [];
     let min_coverage = 80;
     let min_learning = 80;
     let filter_builtin = false;
+    let new_deck_name = "";
 
     async function jpdbRequest(url: string, body: object): Promise<Response> {
         let headers = new Headers();
@@ -59,8 +71,56 @@
         );
         decks.sort((a: Deck, b: Deck) => a.name.localeCompare(b.name)); // sort by name
     }
+
+    async function fetchDeckVocab(deck: Deck): Promise<DeckWithVocab> {
+        const json = await (
+            await jpdbRequest("deck/list-vocabulary", {
+                id: deck.id,
+                fetch_occurences: true,
+            })
+        ).json();
+        let vocabs = [];
+        for (let i = 0; i < json.vocabulary.length; i++) {
+            let vocab: Vocab = {
+                vid: json.vocabulary[i][0],
+                sid: json.vocabulary[i][1],
+                occurences: json.occurences[i],
+            };
+            vocabs.push(vocab);
+        }
+        return { vocabs: vocabs, ...deck };
+    }
+
+    async function addVocabToDeck(id: number, vocabs: Vocab[]) {
+        let vocabulary: [number, number][] = [];
+        let occurences: number[] = [];
+        vocabs.forEach((vocab) => {
+            vocabulary.push([vocab.vid, vocab.sid]);
+            occurences.push(vocab.occurences);
+        });
+        const json = await (
+            await jpdbRequest("deck/add-vocabulary", {
+                id: id,
+                vocabulary: vocabulary,
+                occurences: occurences,
+            })
+        ).json();
+        result = JSON.stringify(json);
+    }
+
+    async function createNewDeck(name: String): Promise<number> {
+        return (
+            await (
+                await jpdbRequest("deck/create-empty", { name: name })
+            ).json()
+        ).id;
+    }
 </script>
 
+<p>
+    Put your API Token here (it won't get send anywhere except jpdb.io). Don't
+    share it with anyone!
+</p>
 <input bind:value={token} type="password" />
 <button type="button" on:click={doPing}>Ping</button>
 
@@ -77,27 +137,69 @@ Min learning coverage:
 Filter builtin:
 <input bind:checked={filter_builtin} type="checkbox" />
 
-<table>
-    <tr>
-        <th>name</th>
-        <th>coverage</th>
-        <th>learning coverage</th>
-        <th>is_built_in</th>
-    </tr>
-    {#each decks as deck}
-        {#if deck.known_coverage > min_coverage && deck.learning_coverage > min_learning && (deck.is_built_in || !filter_builtin)}
+<div class="container">
+    <div>
+        <h3>Decklist</h3>
+        <table>
             <tr>
-                <td>
-                    {deck.name}
-                    <a href="https://jpdb.io/deck?id={deck.id}">[x]</a>
-                </td>
-                <td>{deck.known_coverage.toFixed(1)}</td>
-                <td>{deck.learning_coverage.toFixed(1)}</td>
-                <td>{deck.is_built_in}</td>
+                <th>name</th>
+                <th>coverage</th>
+                <th>learning coverage</th>
+                <th>is_built_in</th>
             </tr>
-        {/if}
-    {/each}
-</table>
+            {#each decks as deck}
+                {#if deck.known_coverage > min_coverage && deck.learning_coverage > min_learning && (deck.is_built_in || !filter_builtin)}
+                    <tr>
+                        <td>
+                            <a
+                                href={"#"}
+                                on:click={() => {
+                                    selected_decks.push(deck);
+                                    selected_decks = selected_decks;
+                                }}
+                                >{deck.name}
+                            </a>
+                            <a href="https://jpdb.io/deck?id={deck.id}"
+                                >[jpdb]</a
+                            >
+                        </td>
+                        <td>{deck.known_coverage.toFixed(1)}</td>
+                        <td>{deck.learning_coverage.toFixed(1)}</td>
+                        <td>{deck.is_built_in}</td>
+                    </tr>
+                {/if}
+            {/each}
+        </table>
+    </div>
+    <div id="deckmerger_div">
+        <h3>Deckmerger</h3>
+        selected decks:<br />
+        {#each selected_decks as deck}
+            <p>{deck.name}</p>
+        {/each}
+        <button
+            on:click={() => {
+                selected_decks = [];
+            }}>Reset selection</button
+        >
+        <p>
+            Name of new deck
+            <input bind:value={new_deck_name} type="text" />
+        </p>
+        <button
+            on:click={async () => {
+                if (new_deck_name !== "") {
+                    let deck_id = await createNewDeck(new_deck_name);
+                    for (const deck of selected_decks) {
+                        const deck_with_vocab = await fetchDeckVocab(deck);
+                        await addVocabToDeck(deck_id, deck_with_vocab.vocabs);
+                        console.log("Adding" + deck.name);
+                    }
+                }
+            }}>create deck</button
+        >
+    </div>
+</div>
 
 <style>
     td:first-child {
@@ -114,5 +216,9 @@ Filter builtin:
     }
     tr {
         border: 1px solid #000000;
+    }
+    .container {
+        display: flex;
+        gap: 50px;
     }
 </style>
