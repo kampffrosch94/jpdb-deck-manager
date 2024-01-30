5fa6687a4e5a22341902a9e3fe69d0403183e29d
kampffrosch94 <kampffrosch94@github.com>
2024-01-30 18:47:33 +0100

    factor out login

--- src/components/DeckManager.svelte
+++ src/components/DeckManager.svelte
@@ -2,8 +2,9 @@
     import DeckList from "./DeckList.svelte";
     import "../util/jpdb_api";
     import { jpdbRequest } from "../util/jpdb_api";
-    import { result, selected_decks, token } from "../state/stores";
+    import { logged_in, result, selected_decks, token } from "../state/stores";
     import DeckMerger from "./DeckMerger.svelte";
+    import Login from "./Login.svelte";
 
     let decks: Deck[] = [];
 
@@ -11,15 +12,6 @@
     let min_coverage = 0;
     let min_learning = 0;
     let filter_builtin = false;
-
-    async function doPing() {
-        const res = await jpdbRequest("ping", {}, $token);
-        const json = await res.json();
-        $result = JSON.stringify(json);
-        if ($result == "{}") {
-            $result = "Token confirmed as correct.";
-        }
-    }
 
     async function fetchDecks() {
         let fields = {
@@ -60,40 +52,24 @@
         );
         decks.sort((a: Deck, b: Deck) => a.name.localeCompare(b.name)); // sort by name
     }
-
 </script>
 
-<p>
-    Put your API Token here (from the bottom of <a
-        href="https://jpdb.io/settings">jpdb.io/settings</a
-    >), it won't get sent anywhere except jpdb.io. Don't share it with anyone!
-</p>
-<p>After you have done this press fetch user decks.</p>
-<p>
-    You can enter the token in your browsers password manager if you don't want
-    to input it every time you use this site.
-</p>
-<input bind:value={$token} type="password" />
-<button type="button" on:click={doPing}>Ping</button>
+{#if !$logged_in}
+    <Login />
+{:else}
+    <button type="button" on:click={fetchDecks}>Fetch user decks</button>
+    Min coverage:
+    <input bind:value={min_coverage} type="number" />
+    Min learning coverage:
+    <input bind:value={min_learning} type="number" />
+    Filter builtin:
+    <input bind:checked={filter_builtin} type="checkbox" />
 
-<p>Result:</p>
-<pre>
-    {$result}
-</pre>
-
-<button type="button" on:click={fetchDecks}>Fetch user decks</button>
-
-Min coverage:
-<input bind:value={min_coverage} type="number" />
-Min learning coverage:
-<input bind:value={min_learning} type="number" />
-Filter builtin:
-<input bind:checked={filter_builtin} type="checkbox" />
-
-<div class="container">
-    <DeckList {decks} {min_coverage} {min_learning} {filter_builtin} />
-    <DeckMerger />
-</div>
+    <div class="container">
+        <DeckList {decks} {min_coverage} {min_learning} {filter_builtin} />
+        <DeckMerger />
+    </div>
+{/if}
 
 <style>
     .container {
--- src/state/stores.ts
+++ src/state/stores.ts
@@ -1,5 +1,6 @@
 import { writable, type Writable } from 'svelte/store';
 
-export const selected_decks: Writable<Deck[]>  = writable([]);
 export const token: Writable<string>  = writable("");
+export const logged_in: Writable<boolean>  = writable(false);
 export const result: Writable<string>  = writable("");
+export const selected_decks: Writable<Deck[]>  = writable([]);
